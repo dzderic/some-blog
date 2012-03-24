f648f8dd67cea76d2636390569624c0ca54eeae4
David Zderic <dzderic@gmail.com>
2012-03-25 00:02:07 +1100

    Added a loading spinner for the markdown preview

--- static/js/new_post.js
+++ static/js/new_post.js
@@ -1,5 +1,6 @@
 $(function() {
   $(".nav-tabs a[href='#tab-preview']").on('show', function() {
+    $("#content-preview").html('<div class="loading-spinner">&nbsp;</div>');
     $.post("/p/md", $("#content").val(), function(data) {
       $("#content-preview").html(data.result);
     }, "json");
